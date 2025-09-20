4d696a50846c4dd6d816473bb001fd11d0bbb62a
Rishikesavan <rishikesavan.amy@gmail.com>
2025-09-20 09:29:24 +0530

    Updates the repo..

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
